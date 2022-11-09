cbd4f9a3b84b326483b529c50d63095c1bf84180
Agustin Nieto <agustin.nieto@ITs-MacBook-Pro.local>
2022-11-09 09:44:16 -0500

    Add those 2 files

---
+++ app/etc/env.php
@@ -0,0 +1,98 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'checkout' => [
+        'async' => 0,
+        'deferred_total_calculating' => 0
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'db' => [
+        'connection' => [
+            'indexer' => [
+                'host' => '127.0.0.1',
+                'dbname' => 'magento2',
+                'username' => 'root',
+                'password' => 'root',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'persistent' => null
+            ],
+            'default' => [
+                'host' => '127.0.0.1',
+                'dbname' => 'magento2',
+                'username' => 'root',
+                'password' => 'root',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ],
+        'table_prefix' => ''
+    ],
+    'crypt' => [
+        'key' => 'fdbd4c684855baade9e33e98189e21ed'
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'developer',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => 'b11_'
+            ],
+            'page_cache' => [
+                'id_prefix' => 'b11_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'lock' => [
+        'provider' => 'db'
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 0,
+        'block_html' => 0,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 0,
+        'target_rule' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'downloadable_domains' => [
+        'local-ee.local'
+    ],
+    'install' => [
+        'date' => 'Wed, 09 Nov 2022 13:49:26 +0000'
+    ]
+];
